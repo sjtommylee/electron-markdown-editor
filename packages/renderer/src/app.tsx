4c48e351fa19dee7c5c2b3b5563b3617a0aad495
sjtommylee <sjtommylee@gmail.com>
2021-11-09 22:29:10 -0800

    react, react-dom

---
+++ packages/renderer/src/app.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './app.css';
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>vite + react</p>
+        {`count is: ${count}`}
+      </header>
+      <button onClick={() => setCount(count => count + 1)}>+</button>
+    </div>
+  );
+};
+
+export default App;
